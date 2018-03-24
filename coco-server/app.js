d6b4cfe5819fc2846dcb70af200e46bdf4b2e80d
Ngoc Nguyen Ba <bangoc.gdg@gmail.com>
2018-03-24 17:38:44 +0800

    Add connect to mongo

--- coco-server/app.js
+++ coco-server/app.js
@@ -1,6 +1,17 @@
+import { config, underscoreId } from './global';
+import mongoose from 'mongoose';
+
 const express = require('express')
 const app = express()
 
+const { port, mongoDBUri } = config.env.dev;
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(80, () => console.log('Example app listening on port 80!'))
+
+app.listen(port, () => {
+    mongoose.connect(mongoDBUri).then(() => {
+        console.log('Conneted to mongoDB at port 27017');
+      });
+    console.log('Example app listening on port 3000!');
+})
